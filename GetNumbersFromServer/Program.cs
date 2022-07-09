5c38df81604d5709f4092ee2f81c4efbcbbba012
OLEG POTAPOV <74708885+Uberion22@users.noreply.github.com>
2022-07-09 15:26:31 +0300

    Corrected grammatical errors

--- GetNumbersFromServer/Program.cs
+++ GetNumbersFromServer/Program.cs
@@ -7,17 +7,17 @@ namespace GetNumbersFromServer
     class Program
     {
         private const int Port = 2013;
-        private const string Server = "88.212.241.115";
-        private const string GreetengsMessage = "Greetings\n";
+        private const string WelcomeMessage = "Greetings\n";
         private const string CheckAdvanced = "Check_Advanced";
         private const string Check = "Check";
         private const float AdvancedResult = 4922031.5f;
-        private const float Result = 925680.5f;
+        private const float Result = 4925680.5f;
 
         static async Task Main(string[] args)
         {
-            var numbersLoader = new NumbersLoader(Server, Port);
-            //numbersLoader.SendMessage(greetengsMessage);
+            var server = Console.ReadLine();
+            var numbersLoader = new NumbersLoader(server, Port);
+            //numbersLoader.SendMessage(WelcomeMessage);
             //numbersLoader.SendMessage(getKeyMessage);
             //var result = await numbersLoader.StartGettingNumbersTasksAsync();
             numbersLoader.SendMessage($"{CheckAdvanced} {AdvancedResult}\n");
